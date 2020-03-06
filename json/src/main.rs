d8f1947af267fabbd3f7556db4035a9a8b6cd99a
jonasrichard <richard.jonas.76@gmail.com>
2020-03-06 13:12:34 +0100

    Very dump json parser

--- json/src/main.rs
+++ json/src/main.rs
@@ -1,75 +1,159 @@
 use std::collections::HashMap;
-use std::io::{self, Read};
-use std::str::Chars;
-
-#[derive(Debug, PartialEq)]
-enum JsonValue {
-    JsonNumber(i32),
-    JsonString(String)
-}
+use std::io::{self, BufReader, Read};
 
 #[derive(Debug, PartialEq)]
 enum Json {
-    JsonArray(Vec<JsonValue>),
+    JsonNumber(i32),
+    JsonString(String),
+    JsonArray(Vec<Json>),
     JsonObject(HashMap<String, Json>)
-}
-
-fn read_stdin() -> String {
-    let mut buffer = String::new();
-
-    match io::stdin().read_to_string(&mut buffer) {
-        Ok(_) =>
-            return buffer,
-        Err(_) =>
-            return "".to_string()
-    }
 }
 
 // TODO skip whitespaces
 
-fn read_array(input: &mut Chars) -> Result<Json, String> {
-    loop {
-        match input.next() {
-            Some(']') =>
-                return Ok(Json::JsonArray(vec![JsonValue::JsonNumber(42)])),
-            Some(_) =>
-                continue,
-            None =>
-                return Err("Array is not terminated".to_string())
+fn read_number(input: &Vec<char>, pos: &mut usize) -> Result<Json, String> {
+    let mut result = String::new();
+
+    while *pos < input.len() {
+        match input[*pos] {
+            c if c >= '0' && c <= '9' =>
+                result.push(c),
+            _ => {
+                *pos -= 1;
+                return Ok(Json::JsonNumber(result.parse::<i32>().unwrap()))
+            }
         }
+        *pos += 1
+    }
+
+    Ok(Json::JsonNumber(result.parse::<i32>().unwrap()))
+}
+
+fn read_string(input: &Vec<char>, pos: &mut usize) -> Result<String, String> {
+    let mut string = String::new();
+
+    // skip the opening "
+    *pos += 1;
+
+    while *pos < input.len() {
+        match input[*pos] {
+            '"' => {
+                return Ok(string)
+            },
+            c =>
+                string.push(c)
+        }
+
+        *pos += 1
+    }
+
+    Err("Reaching the end without closing \"".to_string())
+}
+
+fn read_value(input: &Vec<char>, mut pos: &mut usize) -> Result<Json, String> {
+    match input[*pos] {
+        '"' => {
+            let value = read_string(input, &mut pos)?;
+            return Ok(Json::JsonString(value))
+        },
+        c if c >= '0' && c <= '9' =>
+            read_number(input, &mut pos),
+        _ =>
+            Err("Unknown char".to_string())
     }
 }
 
-fn read_object(input: &mut Chars) -> Result<Json, String> {
-    loop {
-        match input.next() {
-            Some('}') =>
-                return Ok(Json::JsonObject(HashMap::new())),
-            Some(_) =>
-                continue,
-            None =>
-                return Err("Object is not terminated".to_string())
+fn read_array(input: &Vec<char>, pos: &mut usize) -> Result<Json, String> {
+    let mut array = Vec::new();
+
+    while *pos < input.len() {
+        match input[*pos] {
+            ']' =>
+                return Ok(Json::JsonArray(array)),
+            n if n >= '0' && n <= '9' => {
+                match read_number(input, pos) {
+                    Ok(number) =>
+                        array.push(number),
+                    Err(e) =>
+                        return Err(e)
+                }
+            }
+            ',' =>
+                (),
+            _ =>
+                ()
         }
+        *pos += 1
     }
+
+    Err("Reacing the end without ]".to_string())
 }
 
-fn read_json(input: &mut Chars) -> Result<Json, String> {
-    match input.next() {
-        Some('{') =>
-            read_object(input),
-        Some('[') =>
-            read_array(input),
-        Some(_) =>
-            Err("Invalid char".to_string()),
-        None =>
-            Err("Unknown error".to_string())
+fn read_object(input: &Vec<char>, mut pos: &mut usize) -> Result<Json, String> {
+    let mut result = HashMap::new();
+
+    while *pos < input.len() {
+        match input[*pos] {
+            '}' =>
+                return Ok(Json::JsonObject(result)),
+            '"' => {
+                // field name, color and value
+                let name = read_string(&input, &mut pos)?;
+
+                // step over the ':'
+                while *pos < input.len() {
+                    if input[*pos] == ':' {
+                        *pos += 1;
+                        break
+                    }
+                    *pos += 1
+                }
+
+                let value = read_value(&input, &mut pos)?;
+
+                println!("{:?}", value);
+
+                result.insert(name, value);
+            },
+            _ =>
+                ()
+        }
+
+        *pos += 1
     }
+
+    Err("Missing }".to_string())
+}
+
+fn read_json_from_chars(input: &Vec<char>, pos: &mut usize) -> Result<Json, String> {
+    while *pos < input.len() {
+        match input[*pos] {
+            '{' =>
+                return read_object(input, pos),
+            '[' =>
+                return read_array(input, pos),
+            _ =>
+                *pos += 1
+        }
+    }
+
+    Err("Missing data".to_string())
+}
+
+fn read_json(input: String) -> Result<Json, String> {
+    let chars = input.chars().collect();
+    let mut pos: usize = 0;
+
+    return read_json_from_chars(&chars, &mut pos);
 }
 
 fn main() {
-    let input = read_stdin();
+    let mut input_file = BufReader::new(io::stdin());
+    let mut input = String::new();
 
-    read_json(&mut input.chars());
+    input_file.read_to_string(&mut input).expect("Cannot read file");
+
+    read_json(input).expect("json format error");
 }
 
 #[cfg(test)]
@@ -78,8 +162,22 @@ mod tests {
 
     #[test]
     fn test_read_array() -> Result<(), String> {
-        let arr = read_json(&mut "[1,2]".chars())?;
-        assert_eq!(arr, make_array(vec![1, 2]));
+        let array = read_json("[1,2]".to_string()).expect("");
+
+        assert_eq!(array, make_array(vec![1, 2]));
+        Ok(())
+    }
+
+    #[test]
+    fn test_object() -> Result<(), String> {
+        let obj = read_json("{\"field1\":5,\"field2\":\"apple\"}".to_string()).expect("");
+
+        let mut map = HashMap::new();
+        map.insert("field1".to_string(), Json::JsonNumber(5));
+        map.insert("field2".to_string(), Json::JsonString("apple".to_string()));
+
+        let json_obj = Json::JsonObject(map);
+        assert_eq!(obj, json_obj);
         Ok(())
     }
 
@@ -87,7 +185,7 @@ mod tests {
         let mut result = Vec::new();
 
         for i in arr {
-            result.push(JsonValue::JsonNumber(i));
+            result.push(Json::JsonNumber(i));
         }
 
         return Json::JsonArray(result)

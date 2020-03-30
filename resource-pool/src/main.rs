e509f0269031c567f4dfe497797df6b83ca7ddb4
jonasrichard <richard.jonas.76@gmail.com>
2020-03-30 19:54:23 +0200

    Simple resource pool, copy only :(

--- resource-pool/src/main.rs
+++ resource-pool/src/main.rs
@@ -1,43 +1,72 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum PoolItem {
-    Available(Vec<i32>),
+    Available(i32),
     Busy
 }
 
 #[derive(Debug)]
 struct Pool {
-    items: Vec<PoolItem>
+    conns: Vec<PoolItem>
 }
 
 impl Pool {
-    fn new(size: usize) -> Self {
-        let mut items = Vec::new();
+    fn new() -> Self {
+        let mut cs = Vec::new();
 
-        for _ in 0..size {
-            items.push(PoolItem::Available(Vec::new()));
+        for i in 0..4 {
+            cs.push(PoolItem::Available(i));
         }
 
         Pool {
-            items: items
+            conns: cs
         }
     }
 
-    //fn get(&mut self, i: usize) -> Option<Vec<i32>> {
-    //    match self.items[i] {
-    //        PoolItem::Busy =>
-    //            None,
-    //        PoolItem::Available(vec) => {
-    //            self.items[i] = PoolItem::Busy;
-    //            Some((*vec).to_vec())
-    //        }
-    //    }
-    //}
+    fn get(&mut self) -> Option<PoolItem> {
+        match self.conns.iter().position(|e| *e != PoolItem::Busy) {
+            None =>
+                None,
+            Some(pos) => {
+                println!("{}", pos);
+                let (_left, right) = self.conns.split_at_mut(pos);
+
+                match right[0] {
+                    PoolItem::Busy =>
+                        None,
+                    PoolItem::Available(n) => {
+                        right[0] = PoolItem::Busy;
+                        Some(PoolItem::Available(n))
+                    }
+                }
+            }
+        }
+    }
+
+    fn release(&mut self, item: PoolItem) {
+        match item {
+            PoolItem::Available(n) =>
+                match self.conns.iter().position(|e| *e == PoolItem::Busy) {
+                    None =>
+                        // panic
+                        (),
+                    Some(pos) => {
+                        let e = self.conns.get_mut(pos).unwrap();
+                        *e = PoolItem::Available(n);
+                        ()
+                    }
+                },
+            _ =>
+                ()
+        }
+    }
 }
 
 fn main() {
-    let mut pool = Pool::new(4);
+    let mut pool = Pool::new();
 
-    //let mut v2 = pool.get(2);
+    let res2 = pool.get().unwrap();
+    println!("{:?}", pool);
 
+    pool.release(res2);
     println!("{:?}", pool);
 }
